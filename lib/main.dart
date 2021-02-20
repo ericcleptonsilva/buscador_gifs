e2c35498271288e0561559237c4129bd4540d21b
Eric <ericcleptonsilva@gmail.com>
2021-02-20 03:11:34 -0300

    5º app => buscador de Gifs

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:buscador_de_gifs/ui/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
+  _getGifs() async {
+    http.Response response;
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=Bw5uuAnr1PfG4ivVEvo4qMmxsliENL74&limit=25&rating=g");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=Bw5uuAnr1PfG4ivVEvo4qMmxsliENL74&q=$_search&limit=25&offset=$_offset&rating=g&lang=en");
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
